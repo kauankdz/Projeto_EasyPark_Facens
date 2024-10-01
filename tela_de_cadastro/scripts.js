1c5578881668e7069eef1aa17d668ac4759ce343
kauankdz <alienckigame@gmail.com>
2024-10-01 15:50:21 -0300

    comentarios_atualizados

--- tela_de_cadastro/scripts.js
+++ tela_de_cadastro/scripts.js
@@ -33,17 +33,19 @@
         e.target.value = cep; // Atualiza o valor do input
     });
 
-    // Seleciona os elementos do DOM
-const toggleSenha = document.querySelector("#toggleSenha");
-const senhaInput = document.querySelector("#senha");
-const iconOlho = document.querySelector("#iconOlho");
+   
+        // Seleciona os elementos do DOM
+        const toggleSenha = document.querySelector("#toggleSenha");
+        const senhaInput = document.querySelector("#senha");
+        const iconOlho = document.querySelector("#iconOlho");
 
-// Adiciona o evento de clique para alternar a visualização da senha
-toggleSenha.addEventListener("click", function () {
-    // Alterna o tipo do input de "password" para "text" ou vice-versa
-    const tipo = senhaInput.getAttribute("type") === "senha" ? "text" : "senha";
-    senhaInput.setAttribute("type", tipo);
+        // Adiciona o evento de clique para alternar a visualização da senha
+        toggleSenha.addEventListener("click", function () {
+            // Alterna o tipo do input de "password" para "text" ou vice-versa
+            const tipo = senhaInput.getAttribute("type") === "password" ? "text" : "password";
+            senhaInput.setAttribute("type", tipo);
 
-    // Alterna o ícone entre "fa-eye" e "fa-eye-slash"
-    iconOlho.classList.toggle("fa-eye-slash");
-});
+            // Alterna o ícone entre "fa-eye" e "fa-eye-slash"
+            iconOlho.classList.toggle("fa-eye");
+            iconOlho.classList.toggle("fa-eye-slash");
+        });
